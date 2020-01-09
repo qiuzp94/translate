acaa36f92161b0a9cd1a63c7e964f915b207b8c3
qiuzp <qiuzp94@foxmail.com>
2020-01-09 22:10:07 +0800

    新增全局app.js与app.json代码

---
+++ app.js
@@ -0,0 +1,45 @@
+//app.js
+App({
+
+    /**
+     * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
+     */
+    onLaunch: function () {
+        this.globalData.curLang = wx.getStorageSync('curLang') || this.globalData.langList[0];
+    },
+    globalData: {
+        curLang: {},
+        langList: [
+            {
+                'chs': '英语',
+                'lang': 'en',
+                'index': 0
+            },
+            {
+                'chs': '中文',
+                'lang': 'zh',
+                'index': 1
+            },
+            {
+                'chs': '韩语',
+                'lang': 'kor',
+                'index': 2
+            },
+            {
+                'chs': '日语',
+                'lang': 'jp',
+                'index': 3
+            },
+            {
+                'chs': '法语',
+                'lang': 'fra',
+                'index': 4
+            },
+            {
+                'chs': '阿拉伯语',
+                'lang': 'ara',
+                'index': 5
+            }
+        ]
+    }
+})
